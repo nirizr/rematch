40104a16721e75ea8c4405f113122aed26d20d23
Nir Izraeli <nirizr@gmail.com>
2016-08-18 01:59:57 +0300

    Fix flake8 issues with idaplugin utils.py
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- idaplugin/rematch/utils.py
+++ idaplugin/rematch/utils.py
@@ -1,9 +1,6 @@
 import os
-import sys
-
-sys.path += [os.path.join(sys.prefix, "Lib", "site-packages")]
-
 import idc
+
 
 def getPluginPath(*path):
   return os.path.join(idc.GetIdaDirectory(), "plugins", "rematch", *path)
