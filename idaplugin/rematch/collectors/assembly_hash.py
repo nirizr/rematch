a62d73eda2bb64aa393ef9b436ea452db2ff7742
Nir Izraeli <nirizr@gmail.com>
2016-08-20 20:31:50 +0300

    try cleaning up the disassembly
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- idaplugin/rematch/collectors/assembly_hash.py
+++ idaplugin/rematch/collectors/assembly_hash.py
@@ -15,5 +15,9 @@ class AssemblyHashVector(Vector):
     md5 = hashlib.md5()
     for ea in idautils.FuncItems(self.ea):
       asm_line = idc.GetDisasmEx(ea, idc.GENDSM_MULTI_LINE)
+      if ';' in asm_line:
+        asm_line = asm_line[:asm_line.find(';')]
+      asm_line = asm_line.strip()
+      asm_line = " ".join(asm_line.split())
       md5.update(asm_line)
     return md5.hexdigest()
