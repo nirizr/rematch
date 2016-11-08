b4463db263da2137f89e2e82a103f9f64bb4c9ab
Nir <nirizr@gmail.com>
2016-11-07 17:23:43 -0800

    Set allowed hosts back to 'any' in debug mode (#101)
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- server/rematch/settings.py
+++ server/rematch/settings.py
@@ -25,7 +25,17 @@ SECRET_KEY = 'qvc7t@rd5#1l-n_%%&+_fu+-lu#sp2oonf9mto%bn-1#i7$(tu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+# As of django 1.10, allowed hosts are validated in debug as well,
+# this disables that and makes sure all hosts are acceptible when
+# running in debug mode. for more details see
+# https://docs.djangoproject.com/en/1.10/ref/settings/
+# for security implications see
+# https://docs.djangoproject.com/en/1.10/topics/security/#host-headers-virtual-hosting
+if DEBUG:
+  ALLOWED_HOSTS = ['*']
+else:
+  ALLOWED_HOSTS = []
 
 
 # Application definition
