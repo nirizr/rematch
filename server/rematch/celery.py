e3010c5e394792b7811d038ef467da2674bcc836
Shift <shiftre@users.noreply.github.com>
2016-11-21 23:03:26 +0200

    add django.setup() to make celery work.. (#116)
    Signed-off-by: Guy <guy@sentinelone.com>

--- server/rematch/celery.py
+++ server/rematch/celery.py
@@ -3,12 +3,14 @@ from __future__ import absolute_import
 import os
 
 from celery import Celery
+import django
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rematch.settings')
+django.setup()
+
 
 from django.conf import settings  # noqa
-
 app = Celery('rematch')
 
 # Using a string here means the worker will not have to
