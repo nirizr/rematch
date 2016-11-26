544a3f487ac821baf25ea3a86107b3da0f5dc440
Nir Izraeli <nirizr@gmail.com>
2016-11-26 20:28:29 +0200

    Django server to bind to all IP addresses assigned to host
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- server/start_server.sh
+++ server/start_server.sh
@@ -7,4 +7,4 @@ if [ $has_admin -eq 0 ]; then
     echo "Creating admin super user, please enter password"
     python ./manage.py createsuperuser --username admin --email admin@local.com
 fi
-python ./manage.py runserver -v 3
+python ./manage.py runserver -v 3 0.0.0.0:8000
