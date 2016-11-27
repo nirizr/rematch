9d296df54310fa5324a3c60f3a6d9bc2a6d48f5a
Nir <nirizr@gmail.com>
2016-11-27 14:05:22 +0200

    Start server with optional user specified address (#142)
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- server/start_web.sh
+++ server/start_web.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+webserver_addr=$1
+
 python ./manage.py makemigrations collab
 python ./manage.py migrate
 has_admin=$(echo "select count(*) from auth_user where username='admin';" | python ./manage.py dbshell | sed 's/[^0-9]//g')
@@ -7,4 +9,4 @@ if [ $has_admin -eq 0 ]; then
     echo "Creating admin super user, please enter password"
     python ./manage.py createsuperuser --username admin --email admin@local.com
 fi
-python ./manage.py runserver -v 3
+python ./manage.py runserver -v 3 $webserver_addr
