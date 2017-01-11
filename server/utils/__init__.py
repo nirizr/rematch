42e48fcce08b6ba1d789daf39e97f25a8b069009
Nir Izraeli <nirizr@gmail.com>
2017-01-11 22:41:30 +0200

    Add app wide action template lookups (collab/list.html)
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- server/utils/__init__.py
+++ server/utils/__init__.py
@@ -2,8 +2,14 @@ import os
 
 
 class ViewSetTemplateMixin(object):
+  def get_app_name(self):
+    return self.__class__.__module__.lower().replace('.views', '')
+
+  def get_model_name(self):
+    return self.__class__.__name__.lower().replace('viewset', '')
+
   def get_template_names(self):
-    name_parts = [self.__class__.__name__.lower(),
-                  "{}.html".format(self.action)]
-    template_name = os.path.join(*name_parts)
-    return [template_name]
+    page = "{}.html".format(self.action)
+    model_template_name = os.path.join(self.get_model_name(), page)
+    app_template_name = os.path.join(self.get_app_name(), page)
+    return [model_template_name, app_template_name, page]
