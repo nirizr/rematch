8404a9999c5ca7edc68b0fe71bc6e36f2652cbe4
Nir Izraeli <nirizr@gmail.com>
2017-02-04 18:38:18 +0200

    Add index page and final touches to navigation urls handling
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- server/rematch/navigation.py
+++ server/rematch/navigation.py
@@ -1,13 +1,21 @@
 class Menu:
   active_url = None
 
-  def __init__(self, name, url=None, icon=None, *submenu, **kwargs):
+  def __init__(self, name, url=None, icon=None, *submenu):
+    if self.active_url is None:
+      raise RuntimeError("Active url is not set when menu item is created")
+
     self.name = name
     self.icon = icon if icon else ''
-    self.url = url if url else ''
+    self.url = url
     self.submenu = submenu
     self.active = (url == self.active_url or
                    any(sm.active for sm in self.submenu))
+    print(self.name, self.url)
+
+  @classmethod
+  def set_url(cls, url):
+    cls.active_url = '' if url is None else url
 
   @property
   def menu_class(self):
@@ -15,18 +23,23 @@ class Menu:
 
 
 def navigation(request):
-  Menu.active_url = request.resolver_match.url_name
-  print(Menu.active_url)
-  menu = [Menu("Dashboard", icon='fa-dashboard'),
+  Menu.set_url(request.resolver_match.url_name)
+
+  menu = [Menu("Dashboard", 'index', icon='fa-dashboard'),
+
           Menu("Collaboration", None, 'fa-cogs',
                Menu("Projects", url='project-list'),
                Menu("Files", url='file-list'),
                Menu("Tasks", url='task-list'),
-               Menu("Instances", url='instance-list')),
+               Menu("Instances", url='instance-list'))]
 
-          Menu("Account", None, 'fa-desktop',
-               Menu("Your profile", url='profile'),
-               # Menu("Settings", url='settings'),
-               Menu("Logout", url='auth_logout'))]
+  if request.user.is_authenticated():
+    account = Menu("Account", None, 'fa-desktop',
+                   Menu("Your profile", url='profile'),
+                   # Menu("Settings", url='settings'),
+                   Menu("Logout", url='auth_logout'))
+  else:
+    account = Menu("Login", 'auth_login')
+  menu.append(account)
 
   return {'navigation': menu}
--- server/rematch/urls.py
+++ server/rematch/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
     url(r'^collab/', include('collab.urls')),
