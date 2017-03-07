0e657969a7374d06a72882ebe56d9f31e558e0ce
shift <sr@sr.re>
2017-03-06 21:16:25 -0500

    /code -> /rematch_server ; remove never reached call to celery (unless the python webserver crashed, which shoudln't happen)

--- server/start_server.sh
+++ server/start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/code/code/start_web.sh 0.0.0.0:8000 &
-cd /code/code/ && /code/code/start_celery.sh 
+/rematch_server/start_web.sh 0.0.0.0:8000 &
+cd /rematch_server && /rematch_server/start_celery.sh 
--- server/start_web.sh
+++ server/start_web.sh
@@ -2,13 +2,12 @@
 
 webserver_addr=$1
 
-python /code/code/manage.py makemigrations collab
-python /code/code/manage.py migrate
-has_admin=$(echo "select count(*) from auth_user where username='admin';" | python /code/code/manage.py dbshell | sed 's/[^0-9]//g')
+python /rematch_server/manage.py makemigrations collab
+python /rematch_server/manage.py migrate
+has_admin=$(echo "select count(*) from auth_user where username='admin';" | python /rematch_server/manage.py dbshell | sed 's/[^0-9]//g')
 if [ $has_admin -eq 0 ]; then
     echo "Creating admin super user, please enter password"
-    python /code/code/manage.py createsuperuser --username admin --email admin@local.com
+    python /rematch_server/manage.py createsuperuser --username admin --email admin@local.com
 fi
-python /code/code/manage.py runserver -v 3 $webserver_addr
+python /rematch_server/manage.py runserver -v 3 $webserver_addr
 
-celery -A rematch.celery worker -l info
