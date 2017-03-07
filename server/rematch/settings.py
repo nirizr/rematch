ffb1ad2feff1bc96560cf6b83e56d39561b4c0aa
shift <sr@sr.re>
2017-03-06 21:11:45 -0500

    flake8 cosmetics

--- server/rematch/settings.py
+++ server/rematch/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
-import os,sys
+import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,10 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qvc7t@rd5#1l-n_%%&+_fu+-lu#sp2oonf9mto%bn-1#i7$(tu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#if "celeryd" in sys.argv:
-#  DEBUG = False
-#else:
-#  DEBUG = True
+# FIXME
 DEBUG = False
 
 
@@ -39,7 +36,7 @@ DEBUG = False
 if DEBUG:
   ALLOWED_HOSTS = ['*']
 else:
-  ALLOWED_HOSTS = ['127.0.0.1', '172.18.0.*','192*']
+  ALLOWED_HOSTS = ['127.0.0.1', '172.18.0.*', '192*']
 
 
 # Application definition
@@ -118,7 +115,7 @@ DATABASES = {
         'NAME': 'rematch',
         'USER': 'root',
         'PASSWORD': 'qwe123',
-        'HOST' : 'db0_1',
+        'HOST': 'db0_1',
         'PORT': 3306,
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES';",
@@ -184,55 +181,57 @@ REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR', 'redis_1')
 RABBIT_HOSTNAME = os.environ.get('RABBIT_PORT_5672_TCP', 'rabbitmq_1')
 
 
-if RABBIT_HOSTNAME.startswith('tcp://'):  
+if RABBIT_HOSTNAME.startswith('tcp://'):
     RABBIT_HOSTNAME = RABBIT_HOSTNAME.split('//')[1]
 
-BROKER_URL = os.environ.get('BROKER_URL',  
+BROKER_URL = os.environ.get('BROKER_URL',
                             '')
-if not BROKER_URL:  
+if not BROKER_URL:
     BROKER_URL = 'amqp://{user}:{password}@{hostname}/{vhost}/'.format(
         user=os.environ.get('RABBIT_ENV_USER', 'admin'),
         password=os.environ.get('RABBIT_ENV_RABBITMQ_PASS', 'mypass'),
         hostname=RABBIT_HOSTNAME,
         vhost=os.environ.get('RABBIT_ENV_VHOST', ''))
 
-# We don't want to have dead connections stored on rabbitmq, so we have to negotiate using heartbeats
-BROKER_HEARTBEAT = '?heartbeat=30'  
-if not BROKER_URL.endswith(BROKER_HEARTBEAT):  
+# We don't want to have dead connections stored on rabbitmq
+# , so we have to negotiate using heartbeats
+BROKER_HEARTBEAT = '?heartbeat=30'
+if not BROKER_URL.endswith(BROKER_HEARTBEAT):
     BROKER_URL += BROKER_HEARTBEAT
 
-BROKER_POOL_LIMIT = 1  
+BROKER_POOL_LIMIT = 1
 BROKER_CONNECTION_TIMEOUT = 10
 
 # Celery configuration
 
 # configure queues, currently we have only one
-#CELERY_DEFAULT_QUEUE = 'default'  
-#CELERY_QUEUES = (  
+# CELERY_DEFAULT_QUEUE = 'default'
+# CELERY_QUEUES = (
 #    Queue('default', Exchange('default'), routing_key='default'),
-#)
+# )
 
 # Sensible settings for celery
-CELERY_ALWAYS_EAGER = False  
-CELERY_ACKS_LATE = True  
-CELERY_TASK_PUBLISH_RETRY = True  
+CELERY_ALWAYS_EAGER = False
+CELERY_ACKS_LATE = True
+CELERY_TASK_PUBLISH_RETRY = True
 CELERY_DISABLE_RATE_LIMITS = False
 
 # By default we will ignore result
-# If you want to see results and try out tasks interactively, change it to False
+# If you want to see results and try out tasks interactively,
+# change it to False
 # Or change this setting on tasks level
-CELERY_IGNORE_RESULT = True  
-CELERY_SEND_TASK_ERROR_EMAILS = False  
+CELERY_IGNORE_RESULT = True
+CELERY_SEND_TASK_ERROR_EMAILS = False
 CELERY_TASK_RESULT_EXPIRES = 600
 
 # Set redis as celery result backend
-CELERY_RESULT_BACKEND = 'redis://%s:%d/%d' % (REDIS_HOST, REDIS_PORT, REDIS_DB)  
+CELERY_RESULT_BACKEND = 'redis://%s:%d/%d' % (REDIS_HOST, REDIS_PORT, REDIS_DB)
 CELERY_REDIS_MAX_CONNECTIONS = 1
 
 # Don't use pickle as serializer, json is much safer
-CELERY_TASK_SERIALIZER = "json"  
+CELERY_TASK_SERIALIZER = "json"
 CELERY_ACCEPT_CONTENT = ['application/json']
 
-CELERYD_HIJACK_ROOT_LOGGER = False  
-CELERYD_PREFETCH_MULTIPLIER = 1  
-CELERYD_MAX_TASKS_PER_CHILD = 1000 
+CELERYD_HIJACK_ROOT_LOGGER = False
+CELERYD_PREFETCH_MULTIPLIER = 1
+CELERYD_MAX_TASKS_PER_CHILD = 1000
