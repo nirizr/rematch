bb2e2148a259daf4719cb23a0e591c63e8fa396f
shift <sr@sr.re>
2017-03-09 08:19:20 -0500

    start_server.sh is now docker aware

--- server/start_server.sh
+++ server/start_server.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-/rematch_server/start_web.sh 0.0.0.0:8000 &
-cd /rematch_server && /rematch_server/start_celery.sh 
+if [ -n "$IN_DOCKER" ]; then
+  /rematch_server/create_superuser.py 
+  /rematch_server/start_web.sh 0.0.0.0:8000 &
+  cd /rematch_server && /rematch_server/start_celery.sh
+else
+  ./create_superuser.py
+  ./start_web.py 0.0.0.0:8000 &
+  ./start_celery.sh
+fi;
