101c22d263737ef88d57ddb09de13a3d3f1ee02d
shift <sr@sr.re>
2017-03-15 15:15:13 -0400

    wip

--- server/start_server.sh
+++ server/start_server.sh
@@ -3,7 +3,8 @@
 if [ -n "$IN_DOCKER" ]; then
   #/rematch_server/create_superuser.py 
   /rematch_server/start_web.sh 0.0.0.0:8000 &
-  cd /rematch_server && /rematch_server/start_celery.sh
+  cd /rematch_server && /rematch_server/start_celery.sh &&
+   uwsgi --socket :80001 --module rematch.wsgi
 else
   ./create_superuser.sh
   ./start_web.py 0.0.0.0:8000 &
