9229dcbf9b0ffa180036a7bc19efe064c2b8fb6e
shift <sr@sr.re>
2017-03-19 12:12:53 -0400

    wip

--- server/rematch/settings.py
+++ server/rematch/settings.py
@@ -181,6 +181,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Celery configuration
 CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
