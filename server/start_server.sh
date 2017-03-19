704b865f94a613b6b2d920df086ea08c81fa7158
shift <sr@sr.re>
2017-03-19 19:29:59 -0400

    out of the ashes, create_superuser.sh is back, will it work? only @nirizr will tell

--- server/start_server.sh
+++ server/start_server.sh
@@ -4,6 +4,7 @@ if [ -n "$IN_DOCKER" ]; then
   #/rematch_server/create_superuser.py 
   #/rematch_server/start_web.sh 0.0.0.0:8000 &
   cd /rematch_server && /rematch_server/start_celery.sh &
+  ./create_superuser.sh
   rm /etc/nginx/sites-enabled/default
   python /rematch_server/manage.py collectstatic --noinput
   ln -sf /rematch_server/rematch_nginx.conf /etc/nginx/sites-enabled/default
