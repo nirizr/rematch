723cd195c868e5031470df41fb6115624f47624a
shift <sr@sr.re>
2017-03-19 12:28:55 -0400

    codacy trailing whitespace

--- server/rematch/wsgi.py
+++ server/rematch/wsgi.py
@@ -1,8 +1,8 @@
 """
  WSGI config for rematch-server project.
-  
+ 
   It exposes the WSGI callable as a module-level variable named ``application``.
-  
+ 
  For more information on this file, see
  https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
  """
