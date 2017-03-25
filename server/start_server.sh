a8210de25c0895d09d53bbb2bbb744f3b2960178
shift <sr@sr.re>
2017-03-24 21:45:43 -0400

    wip start_server.sh fixup
    wip
    
    things are working if we cheat a bit
    sudo docker-compose exec db1 /bin/bash
    GRANT ALL PRIVILEGES ON *.* TO \'root'@'%\' IDENTIFIED BY \'qwe123QWE WITH GRANT OPTION;
    CREATE DATABASE rematch;
    
    trying to separate redis && rabbitmq to be remote, memory issues+ rebasing
    
    rebasing, start_Sever.sh again
    
    web works
    
    remove unused directory ./docker in ./server/setup/debian (everything is in the root directory of the server)
    
    flake8 cosmetics
    
    start_Server.sh rebasing again
    
    /code -> /rematch_server in docker-compose.yml and moved pip requirements.txt from the current dir nad not putting it
    
    more path fixation + requirements.txt
    
    remove redis lol
    
    possibly fixing the celery leak with a simple if
    
    maybe making the badges more happy
    
    fix typo
    
    rabbitmq_1 only in docker
    
    ugh..
    
    basically adding the whole block to be Docker sensitive, this is a trial and error..
    
    make flake8 great again
    
    comment out heartbeat (hack) to avoid timeouts
    this is something that we should address on future usage to avoid
    high loads and dying connections
    
    rebasing start_server.sh
    
    cleaned Dockerfile + add create_superuser.py
    
    wgsi codacy
    
    flake8
    
     remove create_superuser.* as it's no longer used
    
    rebasing yup

--- server/start_server.sh
+++ server/start_server.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
-tmux new-session -s rematch -d ./start_web.sh
-tmux new-window -t rematch -d ./start_celery.sh
-tmux new-window -t rematch
+if [ -n "$IN_DOCKER" ]; then
+  #/rematch_server/create_superuser.py 
+  #/rematch_server/start_web.sh 0.0.0.0:8000 &
+  cd /rematch_server && /rematch_server/start_celery.sh &
+  rm /etc/nginx/sites-enabled/default
+  python /rematch_server/manage.py collectstatic --noinput
+  ln -sf /rematch_server/rematch_nginx.conf /etc/nginx/sites-enabled/default
+  service nginx start
+  uwsgi --socket :8001 --module rematch.wsgi
+else
+  ./start_web.sh 0.0.0.0:8000 &
+  ./start_celery.sh
+fi
--- server/start_web.sh
+++ server/start_web.sh
@@ -2,11 +2,12 @@
 
 webserver_addr=$1
 
-python ./manage.py makemigrations collab
-python ./manage.py migrate
-has_admin=$(echo "select count(*) from auth_user where username='admin';" | python ./manage.py dbshell | sed 's/[^0-9]//g')
+python /rematch_server/manage.py makemigrations collab
+python /rematch_server/manage.py migrate
+has_admin=$(echo "select count(*) from auth_user where username='admin';" | python /rematch_server/manage.py dbshell | sed 's/[^0-9]//g')
 if [ $has_admin -eq 0 ]; then
     echo "Creating admin super user, please enter password"
-    python ./manage.py createsuperuser --username admin --email admin@local.com
+    python /rematch_server/manage.py createsuperuser --username admin --email admin@local.com
 fi
-python ./manage.py runserver -v 3 $webserver_addr
+python /rematch_server/manage.py runserver -v 3 $webserver_addr
+
