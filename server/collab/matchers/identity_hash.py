f5f0be84de90a52430b9e7f1adaa975ab8efb323
Nir <nirizr@gmail.com>
2017-04-01 00:52:19 +0300

    Fix identity hash name again (#250)
    I'm stupid, previous 'fix' was shit. I need to pay more attention.
    
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- server/collab/matchers/identity_hash.py
+++ server/collab/matchers/identity_hash.py
@@ -4,4 +4,4 @@ from . import hash_matcher
 class IdentityHashMatcher(hash_matcher.HashMatcher):
   vector_type = 'identity_hash'
   match_type = 'identity_hash'
-  matcher_type = 'identity_hash'
+  matcher_name = "Identity Hash"
