532bc7b82959d3fe2770a760e845da5645056f87
Nir <nirizr@gmail.com>
2017-04-26 02:33:38 +0300

    Add server url validation when retrieving bound_file_id (#271)
    by also binding the server alongside the file, and checking current and bound server addresses match
    
    Fixes #16
    
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- idaplugin/rematch/netnode.py
+++ idaplugin/rematch/netnode.py
@@ -1,4 +1,7 @@
+from . import config
+
 import ida_netnode
+import ida_kernwin
 
 
 class NetNode(object):
@@ -10,13 +13,40 @@ class NetNode(object):
   def bound_file_id(self):
     bound_file_id = self._nn.hashstr('bound_file_id')
     if not bound_file_id:
-      return bound_file_id
+      return None
+
+    if not self.validate_bound_server():
+      return None
 
     return int(bound_file_id)
 
+  def validate_bound_server(self):
+    bound_server = self._nn.hashstr('bound_server')
+    if not bound_server:
+      self.bind_server()
+      return True
+
+    if bound_server == config['login']['server']:
+      return True
+
+    msg = ("Current server is '{}' while bound server for opened IDB is '{}'."
+           "Would you like to proceed using currently connected server?\n"
+           "This assumes data from bound server was copied to the current one."
+           "\nclick Yes to bind current server. no to proceed as if file was "
+           "never bound or once to proceed just once using current server."
+           "".format(config['login']['server'], bound_server))
+    ida_kernwin.askbuttons_c("Yes", "No", "Once", 1, msg)
+    # TODO: ask user if server change is acceptable: Yes, No, Once
+
   @bound_file_id.setter
   def bound_file_id(self, file_id):
-    return self._nn.hashset("bound_file_id", str(file_id))
+    r = self._nn.hashset("bound_file_id", str(file_id))
+    if r:
+      self.bind_server()
+    return r
+
+  def bind_server(self):
+    self._nn.hashset("bound_server", str(config['login']['server']))
 
   @bound_file_id.deleter
   def bound_file_id(self):
