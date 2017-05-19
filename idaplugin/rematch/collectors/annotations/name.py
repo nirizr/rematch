823b2fe6b813ce8d2f522077c0be1b36cfc897ff
Nir <nirizr@gmail.com>
2017-05-18 22:07:40 -0700

    Fix error  call (#286)
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- idaplugin/rematch/collectors/annotations/name.py
+++ idaplugin/rematch/collectors/annotations/name.py
@@ -8,7 +8,7 @@ class NameAnnotation(annotation.Annotation):
 
   @classmethod
   def _data(cls, offset):
-    name = ida_name.get_name(offset)
+    name = ida_name.get_name(-1, offset)
     if ida_name.is_uname(name):
       # TODO: get flags here
       return {'name': name, 'flags': 0}
