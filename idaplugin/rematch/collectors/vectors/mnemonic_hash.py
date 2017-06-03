3e110633a050098469209e72ace969fd7b2289ff
Nir <nirizr@gmail.com>
2017-06-03 05:53:43 -0700

    Limit mnemonic hash vector collection to 3 or more assembly instructions (#294)
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- idaplugin/rematch/collectors/vectors/mnemonic_hash.py
+++ idaplugin/rematch/collectors/vectors/mnemonic_hash.py
@@ -13,6 +13,9 @@ class MnemonicHashVector(vector.Vector):
   @classmethod
   def data(cls, offset):
     md5 = hashlib.md5()
+    if len(list(idautils.FuncItems(offset))) < 3:
+      return None
+
     for ea in idautils.FuncItems(offset):
       mnem_line = idc.GetMnem(ea)
       mnem_line = mnem_line.strip()
