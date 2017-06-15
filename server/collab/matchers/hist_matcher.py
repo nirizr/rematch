69b1f6475a2540028ae2f8562fc71c805f404e5e
Nir <nirizr@gmail.com>
2017-06-15 16:16:27 -0400

    Optimise hist matcher score calc and work around django slowdown (#289)
    * Optimise hist matcher score calc and work around django slowdown
    
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>
    
    * clean up a bit
    
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- server/collab/matchers/hist_matcher.py
+++ server/collab/matchers/hist_matcher.py
@@ -1,6 +1,5 @@
 import itertools
 import json
-import time
 
 import numpy as np
 import sklearn as skl
@@ -14,43 +13,30 @@ from . import matcher
 class HistogramMatcher(matcher.Matcher):
   @staticmethod
   def match(source, target):
-    start = time.time()
     source_values = itertools.izip(*source.values_list('instance_id', 'data'))
     target_values = itertools.izip(*target.values_list('instance_id', 'data'))
 
-    print("izip time: {}".format(time.time() - start))
-
     source_instance_ids, source_data = source_values
     target_instance_ids, target_data = target_values
-    print("split time: {}".format(time.time() - start))
 
     source_list = [json.loads(d) for d in source_data]
     target_list = [json.loads(d) for d in target_data]
-    print("load time: {}".format(time.time() - start))
-    print("Source input sample:\t{}".format(source_list[:5]))
-    print("Source input sample:\t{}".format(target_list[:5]))
 
     dictvect = skl.feature_extraction.DictVectorizer()
     source_matrix = dictvect.fit_transform(source_list)
     target_matrix = dictvect.transform(target_list)
-    print("vectorization time: {}".format(time.time() - start))
     print("source matrix: {}, target matrix: {}".format(source_matrix.shape,
                                                         target_matrix.shape))
 
-    source_matrix = skl.preprocessing.normalize(source_matrix, norm='l2')
-    target_matrix = skl.preprocessing.normalize(target_matrix, norm='l2')
-    print("norm time: {}".format(time.time() - start))
-
     distance_matrix = skl.metrics.pairwise.euclidean_distances(source_matrix,
                                                                target_matrix)
-    print("distance time: {}".format(time.time() - start))
-    print("min distance: {}, max distance: {}".format(distance_matrix.min(),
-                                                      distance_matrix.max()))
+    max_distance = distance_matrix.max()
+    score_matrix = (1 - (distance_matrix / max_distance)) * 100
+    print("min, max dist: {}, {}".format(distance_matrix.min(), max_distance))
 
     for source_i, target_i in np.ndindex(*distance_matrix.shape):
       source_instance_id = source_instance_ids[source_i]
       target_instance_id = target_instance_ids[target_i]
 
-      distance = distance_matrix[source_i][target_i]
-      score = (1 - distance) * 100
+      score = score_matrix[source_i][target_i]
       yield (source_instance_id, target_instance_id, score)
