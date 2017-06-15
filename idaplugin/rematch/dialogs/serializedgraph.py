f3bb6b76b8c14c985270a13de2c227857977f7d6
Nir <nirizr@gmail.com>
2017-06-15 16:18:56 -0400

    Fix assembly annotation bugs (#299)
    * Fix assembly annotation bugs
    
    - [x] incorrectly included nodes when dealing with merged function prologs
    - [x] use a list instead of dict when serializing assembly, dicts were
    unnecessary.
    - [ ] name cut-off in displayment of serialized graphs
    
    This requires PR #298 to work
    
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>
    
    * Clean assembly text function offset markers
    
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>
    
    * Fix bugs uncovered by testing
    
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>
    
    * Simplify and fix node function filters by using the proper ida api
    
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>
    
    * Improve regex offset stripdown to include other offest types
    
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- idaplugin/rematch/dialogs/serializedgraph.py
+++ idaplugin/rematch/dialogs/serializedgraph.py
@@ -19,12 +19,12 @@ class SerializedGraphDialog(ida_graph.GraphViewer):
 
     # create nodes
     local_ids = {}
-    for node in self.nodes.values():
+    for node in self.nodes:
       node_text = "\n".join(node['assembly'])
       local_id = self.AddNode((str(node_text), 0xffffff))
       local_ids[node['id']] = local_id
 
-    for node in self.nodes.values():
+    for node in self.nodes:
       local_id = local_ids[node['id']]
       for succ in node['successive']:
         successive_id = local_ids[succ]
