c7c730e75a82650687c83989deb20139cac26e1e
sr <sr.re>
2017-06-17 07:34:49 +0300

    FuzzyMatcher -> FuzzyHashMatcher in apidom_hash.py

--- server/collab/matchers/apidom_hash.py
+++ server/collab/matchers/apidom_hash.py
@@ -1,7 +1,7 @@
 from . import fuzzy_matcher
 
 
-class ApiDominatorMatcher(fuzzy_matcher.FuzzyMatcher):
+class ApiDominatorMatcher(fuzzy_matcher.FuzzyHashMatcher):
   vector_type = 'apidom_hash'
   match_type = 'apidom_hash'
   matcher_name = 'API Call Dominator Hash'
