c5cff18b4fc87d2821820f0738657751cf3f8dd2
Nir <nirizr@gmail.com>
2017-10-03 11:19:31 -0700

    Dont treat flow as code xref when skipping offset bytes (#356)
    Doing so means we're skipping offset bytes for all instructions (except the last one)
    
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- idaplugin/rematch/collectors/vectors/identity_hash.py
+++ idaplugin/rematch/collectors/vectors/identity_hash.py
@@ -30,7 +30,7 @@ class IdentityHashVector(vector.Vector):
     for ea in idautils.FuncItems(offset):
       h = cls._cycle(h, idc.Byte(ea))
       # skip additional bytes of any instruction that contains an offset in it
-      if idautils.CodeRefsFrom(ea, True) or idautils.DataRefsFrom(ea):
+      if idautils.CodeRefsFrom(ea, False) or idautils.DataRefsFrom(ea):
         continue
       for i in range(ea + 1, ea + idc.ItemSize(ea)):
         h = cls._cycle(h, idc.Byte(i))
