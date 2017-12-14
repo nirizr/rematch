nirizr/rematch

commit 09c819114adfa3f7dff18a2ca67710b263840820

Install correct django version in pip, depending on python version (#366)
* Install correct django version in pip, depending on python version
* Copy django>=1.11 adjustments from PR #290
* Fix account view for 1.11/2.0 and improve test a bit
* Set default on delete to cascade
* Remove SessionAuthenticationMiddleware

It provided no functionality since session authentication is unconditionally enabled in Django 1.10

server/accounts/views.py
@@ -1,19 +1,22 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.core.serializers.json import DjangoJSONEncoder
+import json
 
 
 @api_view(['GET'])
 def profile(request):
   user = {"id": request.user.id,
-          "is_authenticated": request.user.is_authenticated(),
+          "is_authenticated": request.user.is_authenticated,
           "is_active": request.user.is_active,
           "is_staff": request.user.is_staff,
           "is_superuser": request.user.is_superuser,
           }
-  if request.user.is_authenticated():
+  if request.user.is_authenticated:
     user.update({"username": request.user.username,
                  "first_name": request.user.first_name,
                  "last_name": request.user.last_name,
                  "email": request.user.email,
                  })
-  return Response(user)
+  response = json.dumps(user, cls=DjangoJSONEncoder)
+  return Response(response)

tests/server/collab/test_all.py
@@ -1,5 +1,6 @@
 import pytest
 from rest_framework import status
+import json
 
 from utils import (rand_hash, create_model, setup_model, assert_eq,
                    assert_response, collab_models_keys)
@@ -66,16 +67,30 @@ def test_model_creation(admin_api_client, admin_user, model_name):
                                    HTTP_ACCEPT='application/json')
 
   assert_response(response, status.HTTP_201_CREATED)
-  projects_created = [response.json()]
+  projects_created = [response.data]
 
   response = admin_api_client.get('/collab/{}/'.format(model_name),
                                   HTTP_ACCEPT="application/json")
-  assert_eq(response.json(), projects_created)
+  assert_eq(response.data, projects_created)
 
 
+# TODO: move to its own file
 def test_template(admin_client):
   response = admin_client.get('/accounts/profile/')
   assert_response(response, status.HTTP_200_OK)
+
+  user_data = json.loads(response.data)
+  assert 'id' in user_data
+  assert 'username' in user_data
+  assert 'email' in user_data
+  assert 'is_authenticated' in user_data
+  assert user_data['is_authenticated'] is True
+  assert 'is_active' in user_data
+  assert user_data['is_active'] is True
+  assert 'is_staff' in user_data
+  assert user_data['is_staff'] is True
+  assert 'is_superuser' in user_data
+  assert user_data['is_superuser'] is True
 
 
 @pytest.mark.django_db

server/collab/models.py
@@ -7,7 +7,7 @@ from collab.validators import idb_validator
 
 class Project(models.Model):
   created = models.DateTimeField(auto_now_add=True)
-  owner = models.ForeignKey(User, db_index=True)
+  owner = models.ForeignKey(User, models.CASCADE, db_index=True)
   name = models.CharField(max_length=256)
   description = models.TextField()
   private = models.BooleanField()
@@ -22,8 +22,9 @@ class Project(models.Model):
 
 class File(models.Model):
   created = models.DateTimeField(auto_now_add=True)
-  owner = models.ForeignKey(User, db_index=True)
-  project = models.ForeignKey(Project, null=True, related_name='files')
+  owner = models.ForeignKey(User, models.CASCADE, db_index=True)
+  project = models.ForeignKey(Project, models.CASCADE, null=True,
+                              related_name='files')
   name = models.CharField(max_length=256)
   description = models.TextField()
   md5hash = models.CharField(max_length=32, db_index=True,
@@ -38,7 +39,7 @@ class File(models.Model):
 
 class FileVersion(models.Model):
   created = models.DateTimeField(auto_now_add=True)
-  file = models.ForeignKey(File, related_name='versions')
+  file = models.ForeignKey(File, models.CASCADE, related_name='versions')
   md5hash = models.CharField(max_length=32,
                              validators=[MinLengthValidator(32)])
 
@@ -60,8 +61,9 @@ class Instance(models.Model):
                   (TYPE_EMPTY_FUNCTION, "Empty Function"),
                   (TYPE_FUNCTION, "Function"))
 
-  owner = models.ForeignKey(User, db_index=True)
-  file_version = models.ForeignKey(FileVersion, related_name='instances')
+  owner = models.ForeignKey(User, models.CASCADE, db_index=True)
+  file_version = models.ForeignKey(FileVersion, models.CASCADE,
+                                   related_name='instances')
   type = models.CharField(max_length=64, choices=TYPE_CHOICES)
   offset = models.BigIntegerField()
   size = models.BigIntegerField()
@@ -89,8 +91,10 @@ class Vector(models.Model):
                   (TYPE_MNEMONIC_HASH, "Mnemonic Hash"),
                   (TYPE_MNEMONIC_HIST, "Mnemonic Hist")]
 
-  instance = models.ForeignKey(Instance, related_name='vectors')
-  file_version = models.ForeignKey(FileVersion, related_name='vectors')
+  instance = models.ForeignKey(Instance, models.CASCADE,
+                               related_name='vectors')
+  file_version = models.ForeignKey(FileVersion, models.CASCADE,
+                                   related_name='vectors')
   type = models.CharField(max_length=64, choices=TYPE_CHOICES)
   type_version = models.IntegerField()
   data = models.TextField()
@@ -119,18 +123,18 @@ class Task(models.Model):
   created = models.DateTimeField(auto_now_add=True)
   finished = models.DateTimeField(null=True)
 
-  owner = models.ForeignKey(User, db_index=True)
+  owner = models.ForeignKey(User, models.CASCADE, db_index=True)
   status = models.CharField(default=STATUS_PENDING, max_length=64,
                             choices=STATUS_CHOICES)
 
-  source_file_version = models.ForeignKey(FileVersion,
+  source_file_version = models.ForeignKey(FileVersion, models.CASCADE,
                                           related_name='source_tasks')
   # TODO: make sure start > end
   source_start = models.BigIntegerField(null=True)
   source_end = models.BigIntegerField(null=True)
   # TODO: make sure only at least one of target_file/target_project is null
-  target_file = models.ForeignKey(File, null=True)
-  target_project = models.ForeignKey(Project, null=True)
+  target_file = models.ForeignKey(File, models.CASCADE, null=True)
+  target_project = models.ForeignKey(Project, models.CASCADE, null=True)
   matchers = models.TextField(default='[]')
 
   progress = models.PositiveSmallIntegerField(default=0)
@@ -138,10 +142,13 @@ class Task(models.Model):
 
 
 class Match(models.Model):
-  from_instance = models.ForeignKey(Instance, related_name='from_matches')
-  to_instance = models.ForeignKey(Instance, related_name='to_matches')
+  from_instance = models.ForeignKey(Instance, models.CASCADE,
+                                    related_name='from_matches')
+  to_instance = models.ForeignKey(Instance, models.CASCADE,
+                                  related_name='to_matches')
 
-  task = models.ForeignKey(Task, db_index=True, related_name='matches')
+  task = models.ForeignKey(Task, models.CASCADE, db_index=True,
+                           related_name='matches')
 
   type = models.CharField(max_length=64, choices=Vector.TYPE_CHOICES)
   score = models.FloatField()
@@ -155,6 +162,7 @@ class Annotation(models.Model):
                   (TYPE_ASSEMBLY, "Assembly"),
                   (TYPE_PROTOTYPE, "Prototype"))
 
-  instance = models.ForeignKey(Instance, related_name='annotations')
+  instance = models.ForeignKey(Instance, models.CASCADE,
+                               related_name='annotations')
   type = models.CharField(max_length=64, choices=TYPE_CHOICES)
   data = models.TextField()

server/rematch/settings/base.py
@@ -63,16 +63,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+# Backwards compat. with django 1.11
+MIDDLEWARE_CLASSES = MIDDLEWARE
 
 ROOT_URLCONF = 'rematch.urls'
 

commit bd9d733b569ca0527db1fb15488d411e20a8f610

Fix login issue introduced in #366 (#372)
* Fix login issue introduced in #366
To support django 1.11/1.10 and handle it's backwards compat BoolCallable

* Use response data for tests

Instead of deserializing output

server/accounts/views.py
@@ -1,22 +1,27 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.core.serializers.json import DjangoJSONEncoder
-import json
 
 
 @api_view(['GET'])
 def profile(request):
+  # workaround django 1.10+ being a BoolCallable instead of an actual boolean
+  # to stay backwards compatible with earlier versions where this attribute
+  # was a method and not a property
+  is_authenticated = request.user.is_authenticated
+  if callable(is_authenticated):
+    is_authenticated = is_authenticated()
+
   user = {"id": request.user.id,
-          "is_authenticated": request.user.is_authenticated,
+          "is_authenticated": is_authenticated,
           "is_active": request.user.is_active,
           "is_staff": request.user.is_staff,
           "is_superuser": request.user.is_superuser,
           }
-  if request.user.is_authenticated:
+  if is_authenticated:
     user.update({"username": request.user.username,
                  "first_name": request.user.first_name,
                  "last_name": request.user.last_name,
                  "email": request.user.email,
                  })
-  response = json.dumps(user, cls=DjangoJSONEncoder)
-  return Response(response)
+
+  return Response(user)

tests/server/collab/test_all.py
@@ -1,6 +1,5 @@
 import pytest
 from rest_framework import status
-import json
 
 from utils import (rand_hash, create_model, setup_model, assert_eq,
                    assert_response, collab_models_keys)
@@ -79,18 +78,17 @@ def test_template(admin_client):
   response = admin_client.get('/accounts/profile/')
   assert_response(response, status.HTTP_200_OK)
 
-  user_data = json.loads(response.data)
-  assert 'id' in user_data
-  assert 'username' in user_data
-  assert 'email' in user_data
-  assert 'is_authenticated' in user_data
-  assert user_data['is_authenticated'] is True
-  assert 'is_active' in user_data
-  assert user_data['is_active'] is True
-  assert 'is_staff' in user_data
-  assert user_data['is_staff'] is True
-  assert 'is_superuser' in user_data
-  assert user_data['is_superuser'] is True
+  assert 'id' in response.data
+  assert 'username' in response.data
+  assert 'email' in response.data
+  assert 'is_authenticated' in response.data
+  assert response.data['is_authenticated'] is True
+  assert 'is_active' in response.data
+  assert response.data['is_active'] is True
+  assert 'is_staff' in response.data
+  assert response.data['is_staff'] is True
+  assert 'is_superuser' in response.data
+  assert response.data['is_superuser'] is True
 
 
 @pytest.mark.django_db
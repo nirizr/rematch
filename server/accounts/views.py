bd9d733b569ca0527db1fb15488d411e20a8f610
Nir <nirizr@gmail.com>
2017-12-14 00:58:00 -0800

    Fix login issue introduced in #366 (#372)
    * Fix login issue introduced in #366
    To support django 1.11/1.10 and handle it's backwards compat BoolCallable
    
    * Use response data for tests
    
    Instead of deserializing output

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -1,22 +1,27 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.core.serializers.json import DjangoJSONEncoder
-import json
 
 
 @api_view(['GET'])
 def profile(request):
+  # workaround django 1.10+ being a BoolCallable instead of an actual boolean
+  # to stay backwards compatible with earlier versions where this attribute
+  # was a method and not a property
+  is_authenticated = request.user.is_authenticated
+  if callable(is_authenticated):
+    is_authenticated = is_authenticated()
+
   user = {"id": request.user.id,
-          "is_authenticated": request.user.is_authenticated,
+          "is_authenticated": is_authenticated,
           "is_active": request.user.is_active,
           "is_staff": request.user.is_staff,
           "is_superuser": request.user.is_superuser,
           }
-  if request.user.is_authenticated:
+  if is_authenticated:
     user.update({"username": request.user.username,
                  "first_name": request.user.first_name,
                  "last_name": request.user.last_name,
                  "email": request.user.email,
                  })
-  response = json.dumps(user, cls=DjangoJSONEncoder)
-  return Response(response)
+
+  return Response(user)
--- tests/server/collab/test_all.py
+++ tests/server/collab/test_all.py
@@ -1,6 +1,5 @@
 import pytest
 from rest_framework import status
-import json
 
 from utils import (rand_hash, create_model, setup_model, assert_eq,
                    assert_response, collab_models_keys)
@@ -79,18 +78,17 @@ def test_template(admin_client):
   response = admin_client.get('/accounts/profile/')
   assert_response(response, status.HTTP_200_OK)
 
-  user_data = json.loads(response.data)
-  assert 'id' in user_data
-  assert 'username' in user_data
-  assert 'email' in user_data
-  assert 'is_authenticated' in user_data
-  assert user_data['is_authenticated'] is True
-  assert 'is_active' in user_data
-  assert user_data['is_active'] is True
-  assert 'is_staff' in user_data
-  assert user_data['is_staff'] is True
-  assert 'is_superuser' in user_data
-  assert user_data['is_superuser'] is True
+  assert 'id' in response.data
+  assert 'username' in response.data
+  assert 'email' in response.data
+  assert 'is_authenticated' in response.data
+  assert response.data['is_authenticated'] is True
+  assert 'is_active' in response.data
+  assert response.data['is_active'] is True
+  assert 'is_staff' in response.data
+  assert response.data['is_staff'] is True
+  assert 'is_superuser' in response.data
+  assert response.data['is_superuser'] is True
 
 
 @pytest.mark.django_db
