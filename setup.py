d3b8a60ecc5ea0233b3871d252f1ed6beae3255e
Nir <nirizr@gmail.com>
2018-05-30 01:42:16 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import sys
 import os
 import re
 from setuptools import setup, find_packages
-try: # for pip >= 10
+try:  # for pip >= 10
   from pip._internal.req import parse_requirements
   from pip._internal.download import PipSession
-except ImportError: # for pip <= 9.0.3
+except ImportError:  # for pip <= 9.0.3
   from pip.req import parse_requirements
   from pip.download import PipSession
 
