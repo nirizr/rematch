16973ac7e929d7a847c3602ee78c3b23e9ee33e3
Nir <nirizr@gmail.com>
2018-05-31 16:09:06 -0700

    Add simple basic block size hist vector & distance matcher (#407)

--- idaplugin/rematch/collectors/vectors/__init__.py
+++ idaplugin/rematch/collectors/vectors/__init__.py
@@ -5,8 +5,9 @@ from .name_hash import NameHashVector
 from .assembly_hash import AssemblyHashVector
 from .mnemonic_hash import MnemonicHashVector
 from .mnemonic_hist import MnemonicHistVector
+from .basicblocksize_hist import BasicBlockSizeHistVector
 
 
 __all__ = ["Vector", "InstructionHashVector", "IdentityHashVector",
            "NameHashVector", "AssemblyHashVector", "MnemonicHashVector",
-           "MnemonicHistVector"]
+           "MnemonicHistVector", "BasicBlockSizeHistVector"]
--- server/collab/matchers/mnemonic_euclidean.py
+++ server/collab/matchers/mnemonic_euclidean.py
@@ -5,6 +5,6 @@ class MnemonicEuclideanMatcher(euclidean_matcher.EuclideanDictionaryMatcher):
   vector_type = 'mnemonic_hist'
   match_type = 'mnemonic_euclidean'
   matcher_name = "Mnemonic Euclidean Distance"
-  matcher_description = ("Matches functions according to thier mnemonic "
+  matcher_description = ("Matches functions according to their mnemonic "
                          "listing using histogram and an euclidean distance "
                          "metric.")
