dcbbd5e24c7b5ec60c79810451c239cc768046a1
Nir <nirizr@gmail.com>
2018-07-18 15:47:41 +0300

    Use MariaDB instead of MySQL (#446)

--- server/manage.py
+++ server/manage.py
@@ -2,6 +2,10 @@
 import os
 import sys
 
+import pymysql
+
+
+pymysql.install_as_MySQLdb()
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rematch.settings.debug")
--- server/rematch/settings/base.py
+++ server/rematch/settings/base.py
@@ -121,10 +121,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'rematch',
         'USER': 'root',
-        'PASSWORD': '',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES';",
-        },
+        'PASSWORD': ''
     }
 }
 
