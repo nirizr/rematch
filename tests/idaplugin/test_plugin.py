d990b398a4d05f7aec4b0dd8b35030831fcb37bb
Nir <nirizr@gmail.com>
2018-08-28 20:03:54 -0700

    Clean and fix action tests (#469)
    This depends on nirizr/pytest-idapro#44

--- tests/idaplugin/test_plugin.py
+++ tests/idaplugin/test_plugin.py
@@ -1,28 +1,5 @@
 import time
-import sys
 import idaplugin
-import pytest
-
-
-class CaptureExceptions(object):
-  def __init__(self):
-    self.exceptions_caught = []
-    self.excepthook = None
-
-  def __enter__(self):
-    self.exceptions_caught = []
-
-    def catch(*except_context):
-      self.exceptions_caught.append(except_context)
-
-    self.excepthook = sys.excepthook
-    sys.excepthook = catch
-    return self.exceptions_caught
-
-  def __exit__(self, *args):
-    sys.excepthook = self.excepthook
-    if self.exceptions_caught:
-      raise self.exceptions_caught[0][1]
 
 
 def test_plugin_creation(idapro_plugin_entry, idapro_app):
@@ -51,14 +28,14 @@ def test_action_creation(idapro_action_entry, idapro_app):
     action.update(ctx)
     idapro_app.processEvents()
 
-  with pytest.raises(Exception) as exc:
-    with CaptureExceptions():
-      action.activate(ctx)
-      time.sleep(1)
-      idapro_app.processEvents()
-    assert isinstance(exc, (NotImplementedError,
-                            idaplugin.rematch.exceptions.NotFoundException,
-                            idaplugin.rematch.exceptions.ConnectionException))
+  try:
+    action.activate(ctx)
+    time.sleep(1)
+    idapro_app.processEvents()
+  except (NotImplementedError,
+          idaplugin.rematch.exceptions.NotFoundException,
+          idaplugin.rematch.exceptions.ConnectionException):
+    pass
 
 
 def test_update(idapro_app):
