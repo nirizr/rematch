42085bed75d40ab8f80e6df58a79d2ef11ae1c39
Nir <nirizr@gmail.com>
2018-10-22 21:13:00 -0700

    fix dependency endpoint (#489)
    * Fix dependencies endpoint access and other minor improvements
    
    * simplify action/dialog interface

--- idaplugin/rematch/actions/base.py
+++ idaplugin/rematch/actions/base.py
@@ -127,12 +127,7 @@ class IDAAction(Action, ida_kernwin.action_handler_t):
     self._running = True
 
     if callable(self.ui_class):
-      self.ui = self.ui_class(accept_handler=self.accept_handler,
-                              reject_handler=self.reject_handler,
-                              finish_handler=self.finish_handler,
-                              submit_handler=self.submit_handler,
-                              response_handler=self.response_handler,
-                              exception_handler=self.exception_handler)
+      self.ui = self.ui_class(self)
       self.ui.show()
     else:
       raise NotImplementedError("activation called on an action class with no "
