68ba369fb56753454e26d375455d574ee06da64d
Nir Izraeli <nirizr@gmail.com>
2018-10-22 21:29:09 -0700

    Make plugin import work

--- idaplugin/plugin_rematch.py
+++ idaplugin/plugin_rematch.py
@@ -1,4 +1,9 @@
-import rematch
+try:
+    # required to run in IDA / python2
+    import rematch
+except ImportError:
+    # required to run in Travis / python3
+    from . import rematch
 
 
 def PLUGIN_ENTRY():  # noqa: N802
