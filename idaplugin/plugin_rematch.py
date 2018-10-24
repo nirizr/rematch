6619ff26b9fa33fbd4808556611b574863435690
Nir <nirizr@gmail.com>
2018-10-24 05:25:29 -0700

    Tighter import rematch exception handling (#497)
    to actually raise all ImportErrors other than the rematch module itself.

--- idaplugin/plugin_rematch.py
+++ idaplugin/plugin_rematch.py
@@ -1,7 +1,10 @@
 try:
     # required to run in IDA / python2
     import rematch
-except ImportError:
+except ImportError as ex:
+    # let all other ImportErrors pass through
+    if not ex.args[0] == "No module named 'rematch'":
+        raise
     # required to run in Travis / python3
     from . import rematch
 
