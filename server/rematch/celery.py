8faf5b0a1496e8045225f1136bfb78360f2df77f
Nir <nirizr@gmail.com>
2018-10-26 21:21:31 -0700

    Fix usage of removed debug django settings (#504)
    * Fix usage of removed debug django settings
    
    Fixes a bug introduced in #501
    
    * fix celery settings too

--- server/rematch/celery.py
+++ server/rematch/celery.py
@@ -3,7 +3,7 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rematch.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rematch.settings.base')
 
 app = Celery('rematch')
 
