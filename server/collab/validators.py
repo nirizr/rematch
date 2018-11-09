76837a8eb16c315cbb73559ba2446ce4c2650cc3
Nir Izraeli <nirizr@gmail.com>
2018-11-08 17:35:22 -0800

    Include magic in IDB magic validation error

--- server/collab/validators.py
+++ server/collab/validators.py
@@ -4,4 +4,5 @@ from django.forms import ValidationError
 def idb_validator(fh):
   magic = fh.read(4)
   if magic not in ("IDA1", "IDA2"):
-    raise ValidationError("file is not a valid IDA database (*.IDB) file.")
+    raise ValidationError("file magic is not a valid IDA database (*.IDB) "
+                          "magic: '{}'.".format(magic))
