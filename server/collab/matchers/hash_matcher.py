3792050e00dbb482bf10b7926d86c9fc65af8fb1
Nir <nirizr@gmail.com>
2018-11-09 19:27:16 -0800

    Remove unneeded target_id from hash matcher query (#526)

--- server/collab/matchers/hash_matcher.py
+++ server/collab/matchers/hash_matcher.py
@@ -5,18 +5,19 @@ from . import matcher
 class HashMatcher(matcher.Matcher):
   @classmethod
   def match(cls, source, target):
+    # TODO: Could be optimized by implementing as a single SQL query
     # unique_values = set(source_dict.values())
     flipped_rest = collections.defaultdict(list)
     # TODO: could be optimized by enumerating all identity matches together
-    target_values = target.values_list('id', 'instance_id', 'data').iterator()
-    for target_id, target_instance_id, target_data in target_values:
+    target_values = target.values_list('instance_id', 'data').iterator()
+    for target_instance_id, target_data in target_values:
       # TODO: could be optimized by uncommenting next line as most 'target'
       # values won't be present in 'source' list
       # if v in unique_values:
-      flipped_rest[target_data].append((target_id, target_instance_id))
+      flipped_rest[target_data].append(target_instance_id)
     source_values = source.values_list('instance_id', 'data').iterator()
     for source_instance_id, source_data in source_values:
       matches = flipped_rest.get(source_data, ())
 
-      for target_id, target_instance_id in matches:
+      for target_instance_id in matches:
         yield (source_instance_id, target_instance_id, 100)
