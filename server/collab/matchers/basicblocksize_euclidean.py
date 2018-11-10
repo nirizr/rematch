f2775721bbf83e732cc238b51da8963ac91c8005
Nir Izraeli <nirizr@gmail.com>
2018-11-10 00:41:02 -0800

    Capitalize letter in basicblock size matcher

--- server/collab/matchers/basicblocksize_euclidean.py
+++ server/collab/matchers/basicblocksize_euclidean.py
@@ -7,7 +7,7 @@ from .euclidean_matcher import EuclideanDictionaryMatcher
 class BasicBlockSizeEuclideanMatcher(EuclideanDictionaryMatcher):
   vector_type = 'basicblocksize_hist'
   match_type = 'basicblocksize_euclidean'
-  matcher_name = "Basic Block size Distance"
+  matcher_name = "Basic Block Size Distance"
   matcher_description = ("Matches functions according to their basic block "
                          "size listing using histogram and an euclidean "
                          "distance metric.")
